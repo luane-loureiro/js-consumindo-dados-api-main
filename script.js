c25d4caa46bae4c3ef671c6413566538c8eec39c
luane-loureiro <luachan@gmail.com>
2024-05-02 16:47:35 -0300

    js complet update

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/21862030/json/');
+var consultaCEP = fetch('https://viacep.com.br/ws/21862032/json/')
+.then(resposta => resposta.json())
+.then(r => {
+    if(r.erro){
+        throw Error ('Esse CEP não existe!')
+    }else {
+        console.log(r)
+    }   
+})
+.catch(erro => console.log(erro));
 
 console.log(consultaCEP);
